7ebb855ba0eecc41f3fb1ab289b7492340eac6b6
Mathieu Bodjikian (backbone) <mathieu@bodjikian.fr>
2014-09-02 18:07:48 +0200

    Fix typo

--- src/wigo/notification.go
+++ src/wigo/notification.go
@@ -14,13 +14,14 @@ type Notification struct {
 	Message		string
 	Date		string
 
-	Host				*Host
 	Hostname			string
 	HostProbesInError	[]string
 
 	OldProbe	*ProbeResult
 	NewProbe	*ProbeResult
 
+    // Private
+    host        *Host
 }
 
 func NewNotification( t string, receiver string, host *Host, oldProbe *ProbeResult, newProbe *ProbeResult) ( this *Notification ){
@@ -31,20 +32,22 @@ func NewNotification( t string, receiver string, host *Host, oldProbe *ProbeResu
 
 	this = new(Notification)
 	this.Type		= t
-	this.Host		= host
 	this.OldProbe	= oldProbe
 	this.NewProbe	= newProbe
-	this.Message 	= fmt.Sprintf("Probe %s switched from %d to %d on host %s", oldProbe.Name, oldProbe.Status, newProbe.Status, host.Name)
+	this.Message	= fmt.Sprintf("Probe %s switched from %d to %d on host %s", oldProbe.Name, oldProbe.Status, newProbe.Status, host.Name)
 	this.Receiver	= receiver
 	this.Hostname	= host.Name
 
 	this.HostProbesInError = host.GetErrorsProbesList()
 
+    // Private 
+    this.host       = host
+
 	return
 }
 
 func (this *Notification) Send( ci chan Event ){
-	this.HostProbesInError = this.Host.GetErrorsProbesList()
+	this.HostProbesInError = this.host.GetErrorsProbesList()
 	this.Date = time.Now().Format(dateLayout)
 	ci <- Event{ SENDNOTIFICATION, this }
 }
