85a3f6f729e805c4bee38439260458e3fd36e3d8
Mathieu Bodjikian (backbone) <mathieu@bodjikian.fr>
2014-09-11 09:13:44 +0200

    0.42

--- src/wigo/global.go
+++ src/wigo/global.go
@@ -40,7 +40,7 @@ func InitWigo() ( err error ){
 
 		LocalWigo.Uuid			= uuid.New()
 		LocalWigo.IsAlive 		= true
-		LocalWigo.Version 		= "Wigo v0.41"
+		LocalWigo.Version 		= "Wigo v0.42"
 		LocalWigo.GlobalStatus 	= 100
 		LocalWigo.GlobalMessage = "OK"
 
