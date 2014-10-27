f0b350372d680394c3387dbaba5bc544374067bd
Mathieu Bodjikian (backbone) <mathieu@bodjikian.fr>
2014-10-27 22:26:00 +0100

    Better

--- build/deb/makePackage.sh
+++ build/deb/makePackage.sh
@@ -2,14 +2,37 @@
 
 set -e
 
-export GOPATH=/root/go:/opt/wigo/repository/
+BUILDDIR=$(pwd)
+PACKAGEROOT=/tmp/wigoBuild
+REPOROOT=$(readlink -f ../..)
+DEBMIRRORROOT=/var/www/mir.root.gg
 
-PACKAGEROOT=/opt/wigo/packages/wigo
-REPOROOT=/opt/wigo/repository
+# Test if we are in the right directory
+if [ ! -e $REPOROOT/src/wigo.go ] ; then 
+    echo "You must be in the build/deb directory of repository to build!"
+    exit 0;
+fi
 
-cd $REPOROOT/src
+# Create
+if [ ! -e $PACKAGEROOT ] ; then
+    rm -fr $PACKAGEROOT
+fi
+
+# Create package subdirs
+mkdir -p $PACKAGEROOT
+mkdir -p $PACKAGEROOT/etc/wigo/conf.d
+mkdir -p $PACKAGEROOT/etc/cron.d
+mkdir -p $PACKAGEROOT/etc/logrotate.d
+mkdir -p $PACKAGEROOT/etc/init.d
+mkdir -p $PACKAGEROOT/usr/local/wigo/lib
+mkdir -p $PACKAGEROOT/usr/local/wigo/etc/conf.d
+mkdir -p $PACKAGEROOT/usr/local/wigo/probes/examples
+mkdir -p $PACKAGEROOT/usr/local/bin
+mkdir -p $PACKAGEROOT/var/lib/wigo
+
 
 # Replace version in code
+cd $REPOROOT/src
 VERSION=$(cat ../VERSION)
 
 sed -i "s/##VERSION##/Wigo v$VERSION/" wigo/global.go
@@ -21,49 +44,39 @@ git checkout wigo/global.go
 cp bin/wigo $PACKAGEROOT/usr/local/wigo/bin
 cp bin/wigocli $PACKAGEROOT/usr/local/bin/wigocli
 
-# Copy lib
-mkdir -p $PACKAGEROOT/usr/local/wigo/lib
-mkdir -p $PACKAGEROOT/var/lib/wigo
+# Copy DEBIAN folder
+cp -R $BUILDDIR/DEBIAN $PACKAGEROOT
 
+# Copy lib
 cp -R $REPOROOT/lib/* $PACKAGEROOT/usr/local/wigo/lib/
 
 # Copy probes
-mkdir -p $PACKAGEROOT/usr/local/wigo/probes/examples
 cp $REPOROOT/probes/examples/* $PACKAGEROOT/usr/local/wigo/probes/examples
 
 # Copy config && probes default config files
-mkdir -p $PACKAGEROOT/etc/wigo/conf.d
-mkdir -p $PACKAGEROOT/usr/local/wigo/etc/conf.d
 cp $REPOROOT/etc/wigo.conf $PACKAGEROOT/usr/local/wigo/etc/wigo.conf.sample
 cp $REPOROOT/etc/conf.d/*.conf $PACKAGEROOT/usr/local/wigo/etc/conf.d
 
 # Copy Init Script
-mkdir -p $PACKAGEROOT/etc/init.d
 cp $REPOROOT/etc/wigo.init $PACKAGEROOT/etc/init.d/wigo
 
 # Copy cron.d
-mkdir -p $PACKAGEROOT/etc/cron.d
 cp $REPOROOT/etc/wigo.cron $PACKAGEROOT/etc/cron.d/wigo
 
 # Copy logrotate
-mkdir -p $PACKAGEROOT/etc/logrotate.d
 cp $REPOROOT/etc/wigo.logrotate $PACKAGEROOT/etc/logrotate.d/wigo
 
 # Copy public directory
-if [ -e "$PACKAGEROOT/usr/local/wigo/public" ] ; then
-    rm -fr $PACKAGEROOT/usr/local/wigo/public
-fi
 cp -R $REPOROOT/public $PACKAGEROOT/usr/local/wigo
 
-
-# Build
-cd /opt/wigo/packages/
-
 # Replace version
-sed -i "s/^Version:.*/Version: $VERSION/" wigo/DEBIAN/control
-
-dpkg-deb --build wigo
+sed -i "s/^Version:.*/Version: $VERSION/" $BUILDDIR/DEBIAN/control
+git checkout $BUILDDIR/DEBIAN/control
 
 # Add to mir
-reprepro --ask-passphrase -b /var/www/mir.root.gg includedeb wheezy /opt/wigo/packages/wigo.deb
-reprepro --ask-passphrase -b /var/www/mir.root.gg includedeb jessie /opt/wigo/packages/wigo.deb
+dpkg-deb --build $PACKAGEROOT $PACKAGEROOT/wigo.deb
+reprepro --ask-passphrase -b $DEBMIRRORROOT includedeb wheezy $PACKAGEROOT/wigo.deb
+reprepro --ask-passphrase -b $DEBMIRRORROOT includedeb jessie $PACKAGEROOT/wigo.deb
+
+# Remove folder
+rm -fr $DEBMIRRORROOT
